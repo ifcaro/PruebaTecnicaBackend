0dabdc96588768c9508766738fd04a5c5abcf756
Ifcaro <ifcaro@ifcaro.net>
2023-09-05 18:16:22 +0200

    Pruebas unitarias

---
+++ Vehicle.Tests/Application/OrderCommandHandlerTest.cs
@@ -0,0 +1,185 @@
+﻿using MediatR;
+using Moq;
+using Vehicle.API.Application.Commands;
+using Vehicle.Domain.AggregatesModel.VehicleAggregate;
+using Xunit;
+
+namespace Vehicle.Tests.Application
+{
+    public class OrderCommandHandlerTest
+    {
+        private readonly Mock<IVehicleRepository> _vehicleRepositoryMock;
+        private readonly Mock<IMediator> _mediator;
+
+        public OrderCommandHandlerTest()
+        {
+            _vehicleRepositoryMock = new Mock<IVehicleRepository>();
+            _mediator = new Mock<IMediator>();
+        }
+
+        [Fact]
+        public async Task Add_Order_return_false_if_vehicle_does_not_exist()
+        {
+            //Arrange   
+            var fakeOrderCmd = FakeAddOrderCommand(new Dictionary<string, object>
+            {
+                ["vehicle"] = Guid.NewGuid(),
+                ["orderId"] = Guid.NewGuid()
+            });
+
+            _vehicleRepositoryMock.Setup(vehicleRepository => vehicleRepository.GetAsync(It.IsAny<Guid>()))
+                .Returns(Task.FromResult((Vehicle.Domain.AggregatesModel.VehicleAggregate.Vehicle)null!));
+
+            _vehicleRepositoryMock.Setup(vehicleRepository => vehicleRepository.UnitOfWork.SaveEntitiesAsync(default))
+                .Returns(Task.FromResult(true));
+
+            //Act
+            var handler = new AddOrderCommandHandler(_vehicleRepositoryMock.Object);
+            var cltToken = new System.Threading.CancellationToken();
+            var result = await handler.Handle(fakeOrderCmd, cltToken);
+
+            //Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async Task Add_Order_return_false_if_order_does_exist()
+        {
+            //Arrange   
+            var vehicleId = Guid.NewGuid();
+            var orderId = Guid.NewGuid();
+
+            var fakeOrderCmd = FakeAddOrderCommand(new Dictionary<string, object>
+            {
+                ["vehicle"] = vehicleId,
+                ["orderId"] = orderId
+            });
+
+            _vehicleRepositoryMock.Setup(vehicleRepository => vehicleRepository.GetAsync(It.IsAny<Guid>()))
+                .Returns(Task.FromResult(FakeVehicleWithOrder(vehicleId, orderId)));
+
+            _vehicleRepositoryMock.Setup(vehicleRepository => vehicleRepository.UnitOfWork.SaveEntitiesAsync(default))
+                .Returns(Task.FromResult(true));
+
+            //Act
+            var handler = new AddOrderCommandHandler(_vehicleRepositoryMock.Object);
+            var cltToken = new System.Threading.CancellationToken();
+            var result = await handler.Handle(fakeOrderCmd, cltToken);
+
+            //Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async Task Add_Order_return_false_if_order_does_exist_in_other_vehicle()
+        {
+            //Arrange   
+            var vehicleId = Guid.NewGuid();
+            var orderId = Guid.NewGuid();
+
+            var fakeOrderCmd = FakeAddOrderCommand(new Dictionary<string, object>
+            {
+                ["vehicle"] = vehicleId,
+                ["orderId"] = orderId
+            });
+
+            _vehicleRepositoryMock.Setup(vehicleRepository => vehicleRepository.GetAsync(It.IsAny<Guid>()))
+                .Returns((Guid x) => Task.FromResult(FakeVehicle(x)));
+
+            _vehicleRepositoryMock.Setup(vehicleRepository => vehicleRepository.GetByOrderIdAsync(It.IsAny<Guid>()))
+                .Returns(Task.FromResult(FakeVehicleWithOrder(Guid.NewGuid(), orderId)));
+
+            _vehicleRepositoryMock.Setup(vehicleRepository => vehicleRepository.UnitOfWork.SaveEntitiesAsync(default))
+                .Returns(Task.FromResult(true));
+
+            //Act
+            var handler = new AddOrderCommandHandler(_vehicleRepositoryMock.Object);
+            var cltToken = new System.Threading.CancellationToken();
+            var result = await handler.Handle(fakeOrderCmd, cltToken);
+
+            //Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async Task Remove_Order_return_false_if_vehicle_does_not_exist()
+        {
+            //Arrange   
+            var fakeOrderCmd = FakeRemoveOrderCommand(new Dictionary<string, object>
+            {
+                ["vehicle"] = Guid.NewGuid(),
+                ["orderId"] = Guid.NewGuid()
+            });
+
+            _vehicleRepositoryMock.Setup(vehicleRepository => vehicleRepository.GetAsync(It.IsAny<Guid>()))
+                .Returns(Task.FromResult((Vehicle.Domain.AggregatesModel.VehicleAggregate.Vehicle)null!));
+
+            _vehicleRepositoryMock.Setup(vehicleRepository => vehicleRepository.UnitOfWork.SaveEntitiesAsync(default))
+                .Returns(Task.FromResult(true));
+
+            //Act
+            var handler = new RemoveOrderCommandHandler(_vehicleRepositoryMock.Object);
+            var cltToken = new System.Threading.CancellationToken();
+            var result = await handler.Handle(fakeOrderCmd, cltToken);
+
+            //Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async Task Remove_Order_return_false_if_order_does_not_exist()
+        {
+            //Arrange   
+            var vehicleId = Guid.NewGuid();
+            var orderId = Guid.NewGuid();
+
+            var fakeOrderCmd = FakeRemoveOrderCommand(new Dictionary<string, object>
+            {
+                ["vehicle"] = vehicleId,
+                ["orderId"] = orderId
+            });
+
+            _vehicleRepositoryMock.Setup(vehicleRepository => vehicleRepository.GetAsync(It.IsAny<Guid>()))
+                .Returns(Task.FromResult(FakeVehicle(vehicleId)));
+
+            _vehicleRepositoryMock.Setup(vehicleRepository => vehicleRepository.UnitOfWork.SaveEntitiesAsync(default))
+                .Returns(Task.FromResult(true));
+
+            //Act
+            var handler = new RemoveOrderCommandHandler(_vehicleRepositoryMock.Object);
+            var cltToken = new System.Threading.CancellationToken();
+            var result = await handler.Handle(fakeOrderCmd, cltToken);
+
+            //Assert
+            Assert.False(result);
+        }
+
+        private Vehicle.Domain.AggregatesModel.VehicleAggregate.Vehicle FakeVehicle(Guid vehicleId)
+        {
+            return new Vehicle.Domain.AggregatesModel.VehicleAggregate.Vehicle(vehicleId);
+        }
+
+        private Vehicle.Domain.AggregatesModel.VehicleAggregate.Vehicle FakeVehicleWithOrder(Guid vehicleId, Guid orderId)
+        {
+            var vehicle = new Vehicle.Domain.AggregatesModel.VehicleAggregate.Vehicle(vehicleId);
+
+            vehicle.AddOrder(orderId);
+
+            return vehicle;
+        }
+
+        private AddOrderCommand FakeAddOrderCommand(Dictionary<string, object> args = null!)
+        {
+            return new AddOrderCommand(
+                vehicle: args != null && args.ContainsKey("vehicle") ? (Guid)args["vehicle"] : Guid.Empty,
+                orderId: args != null && args.ContainsKey("orderId") ? (Guid)args["orderId"] : Guid.Empty);
+        }
+
+        private RemoveOrderCommand FakeRemoveOrderCommand(Dictionary<string, object> args = null!)
+        {
+            return new RemoveOrderCommand(
+                vehicle: args != null && args.ContainsKey("vehicle") ? (Guid)args["vehicle"] : Guid.Empty,
+                orderId: args != null && args.ContainsKey("orderId") ? (Guid)args["orderId"] : Guid.Empty);
+        }
+    }
+}
---
+++ Vehicle.Tests/Domain/VehicleAggregateTest.cs
@@ -0,0 +1,54 @@
+﻿using Xunit;
+
+namespace Vehicle.Tests.Domain
+{
+    public class VehicleAggregateTest
+    {
+        [Fact]
+        public void Add_vehicle_location_sets_current_location_and_history()
+        {
+            //Arrange    
+            var vehicleId = Guid.NewGuid();
+            var location = "test_location";
+
+            //Act 
+            var fakeVehicle = new Vehicle.Domain.AggregatesModel.VehicleAggregate.Vehicle(Guid.NewGuid());
+            fakeVehicle.SetCurrentLocation(location);
+
+            //Assert
+            Assert.Equal(fakeVehicle.CurrentLocation, location);
+            Assert.Contains(fakeVehicle.LocationHistory, x => x.Coordinates == location);
+        }
+
+        [Fact]
+        public void Add_order_sets_date_added()
+        {
+            //Arrange    
+            var vehicleId = Guid.NewGuid();
+            var orderId = Guid.NewGuid();
+
+            //Act 
+            var fakeVehicle = new Vehicle.Domain.AggregatesModel.VehicleAggregate.Vehicle(Guid.NewGuid());
+            fakeVehicle.AddOrder(orderId);
+
+            //Assert
+            Assert.Contains(fakeVehicle.Orders, x => x.OrderId == orderId && x.DateAdded != default && x.DateRemoved == null);
+        }
+
+        [Fact]
+        public void Remove_order_sets_date_removed()
+        {
+            //Arrange    
+            var vehicleId = Guid.NewGuid();
+            var orderId = Guid.NewGuid();
+
+            //Act 
+            var fakeVehicle = new Vehicle.Domain.AggregatesModel.VehicleAggregate.Vehicle(Guid.NewGuid());
+            fakeVehicle.AddOrder(orderId);
+            fakeVehicle.RemoveOrder(orderId);
+
+            //Assert
+            Assert.Contains(fakeVehicle.Orders, x => x.OrderId == orderId && x.DateAdded != default && x.DateRemoved != null);
+        }
+    }
+}
